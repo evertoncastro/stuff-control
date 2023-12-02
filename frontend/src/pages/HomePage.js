b2b96b5244b28157c49c846793a5d7cbba1a79f6
Everton Castro <contato@evertoncastro.com>
2023-12-02 16:58:08 -0300

    Tailwind sertup

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends Component {
   render() {
     return (
       <main className="container">
-        <h1 className="text-uppercase text-center my-4">Home page</h1>
+        <h1 className="text-3xl font-bold underline">Home page</h1>
         <Link to="/expenses">Expenses</Link>
         <Link to="/login">Login</Link>
         <Link to="/logout">Logout</Link>
