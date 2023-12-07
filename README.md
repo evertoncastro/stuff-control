31bd9c6ec0b5dfda2b357fde233614b0072badb0
Everton Castro <contato@evertoncastro.com>
2023-12-07 18:27:05 -0300

    Django serving react bundle

---
+++ README.md
@@ -0,0 +1,9 @@
+Control stuff
+
+
+
+#### References
+- https://fractalideas.com/blog/making-react-and-django-play-well-together/
+
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App'
 import { SidebarProvider } from './context/SidebarContext'
 import ThemedSuspense from './components/ThemedSuspense'
 import { Windmill } from '@windmill/react-ui'
-import * as serviceWorker from './serviceWorker'
 
 
 ReactDOM.render(
@@ -19,7 +18,3 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
