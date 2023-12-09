2a8ab7abcb5e53311cc1f979df659f18383fadca
Everton Castro <contato@evertoncastro.com>
2023-12-09 12:47:02 -0300

    Modifying infra scripts

--- infra/build.sh
+++ infra/build.sh
@@ -6,7 +6,6 @@ source $NVM_DIR/nvm.sh;
 nvm use 16
 
 cd frontend
-rm -rf build
 yarn build
 
 mkdir -p build/root/
@@ -17,6 +16,5 @@ done
 cd ..
 
 poetry env use python3.11
-rm -rf application/static/
 python manage.py collectstatic --no-input
 
